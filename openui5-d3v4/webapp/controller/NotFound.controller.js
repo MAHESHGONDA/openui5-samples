d0a638de8883657c7d889dcb225f6625544eb444
MAHESHGONDA <MAHESHMALKNOOR@GMAIL.COM>
2019-12-26 11:38:17 +0000

    save

--- openui5-d3v4/webapp/controller/NotFound.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
-   "use strict";
-   return Controller.extend("com.d3.demo.controller.NotFound", {
-      onInit: function () {
-      }
-   });
-});
